29756df4af9ab1aee2d23bcdb84a8bf2a0efa9d7
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-12-04 12:30:39 -0600

    Svelte Project:Section 13 More Reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,29 @@
 <script>
-	let name = 'Garrett'
+	let name = 'Garrett';
 	let age = 33;
+	
+	$: upppercaseName = name.toUpperCase();
+	$: console.log(name);
+
+	$: if(name === 'William'){
+		age = 33;
+	}
 
 	function incrementAge(){
 		age += 1;
 	}
+
+	function changeName(){
+		name = "William";
+	}
 </script>
+
+<h1>Hello I am {upppercaseName}, and I {age} years old!</h1>
+<button on:click={incrementAge}>Change age</button>
+<button on:click={changeName}>Change Name</button>
 
 <style>
 	h1 {
 		color: purple;
 	}
 </style>
-
-<h1>Hello I am {name}, and I {age} years old!</h1>
-<button on:click={incrementAge}>Change age</button>
